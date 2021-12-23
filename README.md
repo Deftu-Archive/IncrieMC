fffa6f79109298d826d234363d6cb46aa4777f54
Deftu <deftudev@gmail.com>
2021-12-23 15:31:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Deftu's MinecraftForge mod template.
+## Available for 1.8.9 and 1.12.2.
---
+++ src/main/java/xyz/deftu/template/ForgeTemplate.java
@@ -0,0 +1,8 @@
+package xyz.deftu.template;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(name = ModInfo.NAME, version = ModInfo.VERSION, modid = ModInfo.ID)
+public class ForgeTemplate {
+    // TODO - Your mod's logic.
+}
